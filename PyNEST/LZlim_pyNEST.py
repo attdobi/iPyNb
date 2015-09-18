3c85ec0dc48a510d7f675383f162127692de6636
Attila Dobi <adobi@pdsf8.nersc.gov>
2015-09-18 10:33:50 -0700

    updates for pyNEST

--- PyNEST/LZlim_pyNEST.py
+++ PyNEST/LZlim_pyNEST.py
@@ -26,7 +26,7 @@ def dN2NphNe(ParticleType='ER',file_path='data/PP_7Be_evt_ton_year_keV_lin_noDis
     '''input: ParticleType('ER' or 'NR'), file path to diff spectrum, total rate in evts/kg/day, drift field V/cm, g1, SPE_res, electron extraction efficiency, SE size, sigSE, electron lifetime, center of detector)'''
     #nSim=ceil(total_rate*5600*1000)# use total_rate= Calc_Rate_evts_kg_day to Simulate 1 nominal LZ exposure
     Edatatxt, rate=np.loadtxt(file_path,skiprows=0,unpack=True) #evts/ton/year #Energy scale must be linear in text file for code to work properly!
-    Edata=np.arange(min(Edatatxt),max(Edatatxt),0.01)#0.01 keV binning
+    Edata=np.arange(min(Edatatxt),max(Edatatxt),0.001)#0.01 keV binning
     dR = rate/1000/365 #convert from Ton to kg and year to days 
     dR=np.interp(Edata,Edatatxt,dR)
     Rcum = dR.cumsum()/dR.sum()  # Energy cumulative distribution function
@@ -38,19 +38,20 @@ def dN2NphNe(ParticleType='ER',file_path='data/PP_7Be_evt_ton_year_keV_lin_noDis
     r_uniform = np.random.rand(nSim)
     Eee = np.interp(r_uniform, Rcum, Eee)
     Nph, Ne = pn.Nph_Ne(ParticleType,f_drift*np.ones_like(Eee),Eee)
-    S1 = st.binom.rvs(array(Nph, dtype=int64),g1) #binomial photon collection
-    NS1_coin=st.binom.rvs(S1,1-math.erfc(((1-minSpikePE)/SPE_res)/sqrt(2))/2) #takes at least minSpikePE to produce a spike, use binomial probability calculated from erfc
-    S1 = st.norm.rvs(S1,sqrt(S1)*SPE_res,size=size(S1)) #single PE resolution with sigma=sqrt(N)*sig_PE
+    S1_spike = st.binom.rvs(array(Nph, dtype=int64),g1) #binomial photon collection
+    NS1_coin=st.binom.rvs(S1_spike,1-math.erfc(((1-minSpikePE)/SPE_res)/sqrt(2))/2) #takes at least minSpikePE to produce a spike, use binomial probability calculated from erfc
+    S1 = st.norm.rvs(S1_spike,sqrt(S1_spike)*SPE_res,size=size(S1_spike)) #single PE resolution with sigma=sqrt(N)*sig_PE
     Ne_ext = st.binom.rvs(array(Ne*exp(-dt0/e_lifetime), dtype=int64), eff_extract)
     S2 = st.norm.rvs(Ne_ext*SE_size,sqrt(SE_res**2 * Ne_ext),size=size(Ne_ext))
-    S2 = S2*exp(dt0/e_lifetime)
+    S2 = S2*exp(dt0/e_lifetime)/eff_extract
 
+    Eee_min = min(Edata) #for integral
     Eee_max = max(Edata) #for integral
-    Eee_vect = np.linspace(0,Eee_max*1.01,2e3)
+    Eee_vect = np.linspace(Eee_min,Eee_max*1.01,2e6)
     dR_vect = np.interp(Eee_vect[Eee_vect>=0.1],Edata[Edata>=0.1],dR[Edata>=0.1]) # Start integrating at 0.1 keV
     Rate_evts_kg_day = float(dR_vect.sum() * np.diff(Eee_vect[:2])) #evts/kg/day 100% acceptance
     LZ_exposure_factor=nSim/(Rate_evts_kg_day*5600*1000)
-    print('total rate [evts/kg/day] = {:g}'.format(Rate_evts_kg_day)) #evts/kg/day
+    print('total rate above {:.2f} keV = {:g} [evts/kg/day]'.format(Eee_min, Rate_evts_kg_day)) #evts/kg/day
     print('LZ exposure factor = {:.1f}'.format(LZ_exposure_factor)) #evts/kg/day
 
     plt.figure()
@@ -66,42 +67,56 @@ def dN2NphNe(ParticleType='ER',file_path='data/PP_7Be_evt_ton_year_keV_lin_noDis
     #text(1,0.013,'PP+7Be',fontsize=16)
     plt.rcParams.update({'font.size': 18})
 
-    return Nph, Ne, S1, S2, NS1_coin, Rate_evts_kg_day, LZ_exposure_factor
+    return Nph, Ne, Ne_ext, S1_spike, S1, S2, NS1_coin, Rate_evts_kg_day, LZ_exposure_factor
     
 '''Define function to generate flat ER and NR bands'''    
-def genBands(nSim=1e5,maxS1=50,f_drift=700,g1=0.075,SPE_res= 0.5,eff_extract=0.95,SE_size=50,SE_res=sqrt(50),e_lifetime=1000, dt0=500):
+def genBands(nSim=1e5,maxS1=50,f_drift=700,g1=0.075,SPE_res= 0.5,eff_extract=0.95,SE_size=50,SE_res=sqrt(50),e_lifetime=1000, dt0=500,minSpikePE=0.25,min_NS1_coin=3, min_Ne_ext=5 ):
     #Calculate the NR band, and count below that for acceptance ########
     maxEr=100 #keVnr, for flat spectrum... DD
     Flat_Er = maxEr*st.uniform.rvs(size=nSim); #0-100 keVnr
-    Nph_flat, Ne_flat= pn.Nph_Ne('NR',f_drift*np.ones_like(Flat_Er),Flat_Er)
-    S1_flat = st.binom.rvs(array(Nph_flat, dtype=int64),g1) #mod g1 with spe
-    Ne_flat_ext = st.binom.rvs(array(Ne_flat*exp(-dt0/e_lifetime), dtype=int64), eff_extract)
-    S2_flat = st.norm.rvs(Ne_flat_ext*SE_size,sqrt(SE_res**2 * Ne_flat_ext),size=size(Ne_flat_ext))
-    S2_flat = S2_flat*exp(dt0/e_lifetime)
+    Nph, Ne= pn.Nph_Ne('NR',f_drift*np.ones_like(Flat_Er),Flat_Er)
+    S1_spike = st.binom.rvs(array(Nph, dtype=int64),g1) #binomial photon collection
+    NS1_coin=st.binom.rvs(S1_spike,1-math.erfc(((1-minSpikePE)/SPE_res)/sqrt(2))/2) #takes at least minSpikePE to produce a spike, use binomial probability calculated from erfc
+    S1 = st.norm.rvs(S1_spike,sqrt(S1_spike)*SPE_res,size=size(S1_spike)) #single PE resolution with sigma=sqrt(N)*sig_PE
+    Ne_ext = st.binom.rvs(array(Ne*exp(-dt0/e_lifetime), dtype=int64), eff_extract)
+    S2 = st.norm.rvs(Ne_ext*SE_size,sqrt(SE_res**2 * Ne_ext),size=size(Ne_ext))
+    S2 = S2*exp(dt0/e_lifetime)/eff_extract
     S1_bins=linspace(1,maxS1,maxS1)
-    mean_S2oS1_flat_n=empty_like(S1_bins)
+    S1_bin_cen_n=empty_like(S1_bins)
+    mean_S2oS1_n=empty_like(S1_bins)
+    std_S2oS1_n=empty_like(S1_bins)
     #Find the NR S2/S1 band at each S1
+    det_cuts= (NS1_coin>=min_NS1_coin) & (Ne_ext>=min_Ne_ext)
     for index, S1s in enumerate(S1_bins):
-        mean_S2oS1_flat_n[index]=mean(S2_flat[S1_flat==S1s]/S1s)
-    
+        cut=det_cuts & inrange(S1,[S1s-0.5,S1s+0.5])
+        S1_bin_cen_n[index]=mean(S1[cut])
+        mean_S2oS1_n[index]=mean(log10(S2[cut]/S1[cut]))
+        std_S2oS1_n[index]=std(log10(S2[cut]/S1[cut]))
+        
     #Calculate the ER band #################################
     maxEe=100 #keVee, for flat ER spectrum
     Flat_Ee = maxEe*st.uniform.rvs(size=nSim); #0-100 keVee
-    Nph_flat_e, Ne_flat_e= pn.Nph_Ne('ER',f_drift*np.ones_like(Flat_Ee),Flat_Ee)
-    S1_e = st.binom.rvs(array(Nph_flat_e, dtype=int64),g1) #mod g1 with spe
-    Ne_e_ext = st.binom.rvs(array(Ne_flat_e*exp(-dt0/e_lifetime), dtype=int64), eff_extract)
-    S2_e = st.norm.rvs(Ne_e_ext*SE_size,sqrt(SE_res**2 * Ne_e_ext),size=size(Ne_e_ext))
-    S2_e_c = S2_e*exp(dt0/e_lifetime)
+    Nph, Ne= pn.Nph_Ne('ER',f_drift*np.ones_like(Flat_Ee),Flat_Ee)
+    S1_spike = st.binom.rvs(array(Nph, dtype=int64),g1) #binomial photon collection
+    NS1_coin=st.binom.rvs(S1_spike,1-math.erfc(((1-minSpikePE)/SPE_res)/sqrt(2))/2) #takes at least minSpikePE to produce a spike, use binomial probability calculated from erfc
+    S1 = st.norm.rvs(S1_spike,sqrt(S1_spike)*SPE_res,size=size(S1_spike)) #single PE resolution with sigma=sqrt(N)*sig_PE
+    Ne_ext = st.binom.rvs(array(Ne*exp(-dt0/e_lifetime), dtype=int64), eff_extract)
+    S2 = st.norm.rvs(Ne_ext*SE_size,sqrt(SE_res**2 * Ne_ext),size=size(Ne_ext))
+    S2 = S2*exp(dt0/e_lifetime)/eff_extract
     S1_bins=linspace(1,maxS1,maxS1)
+    S1_bins=linspace(1,maxS1,maxS1)
+    S1_bin_cen_e=empty_like(S1_bins)
     mean_S2oS1_e=empty_like(S1_bins)
     stdev_S2oS1_e=empty_like(S1_bins)
     #Find the ER S2/S1 band at each S1
+    det_cuts= (NS1_coin>=min_NS1_coin) & (Ne_ext>=min_Ne_ext)
     for index, S1s in enumerate(S1_bins):
-        s1_cut=[(S1_e==S1s) & (S2_e_c>0)]
-        mean_S2oS1_e[index]=mean(log10(S2_e_c[s1_cut]/S1s))
-        stdev_S2oS1_e[index]=std(log10(S2_e_c[s1_cut]/S1s))
-    
-    return S1_bins, mean_S2oS1_flat_n, mean_S2oS1_e, stdev_S2oS1_e
+        cut=det_cuts & inrange(S1,[S1s-0.5,S1s+0.5])
+        S1_bin_cen_e[index]=mean(S1[cut])
+        mean_S2oS1_e[index]=mean(log10(S2[cut]/S1[cut]))
+        stdev_S2oS1_e[index]=std(log10(S2[cut]/S1[cut]))
+
+    return S1_bin_cen_n, mean_S2oS1_n, std_S2oS1_n, S1_bin_cen_e, mean_S2oS1_e, stdev_S2oS1_e
     
 
 def E2NphNe(ParticleType='ER',Energy = linspace(1,100,1000),f_drift=700,g1=0.075,SPE_res= 0.5,eff_extract=0.95,SE_size=50,SE_res=sqrt(50),e_lifetime=1000, dt0=500):
